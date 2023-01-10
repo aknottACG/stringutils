8c8db50f280b280719cd7e43838df0ba710ccc27
aknott <aknott2210@gmail.com>
2023-01-10 06:58:40 -0500

    fixing return values

--- string_utils.go
+++ string_utils.go
@@ -2,10 +2,10 @@ package stringutils
 
 import "strings"
 
-func Upper(value string) {
+func Upper(value string) string {
 		return strings.ToUpper(value)
 }
 
-func Lower(value string) {
+func Lower(value string) string {
 	return strings.ToLower(value)
 }
